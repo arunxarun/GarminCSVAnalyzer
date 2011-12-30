67f19e4af3cf59dd6e1c87dbbbece38056b9acc4
Arun Jacob <arun.x.arun@gmail.com>
2011-12-30 06:12:14 -0800

    further test refinement
    testing randomly generated centroids to make sure they reside within
    min and max values of each dimension

--- test/kmeanstest.py
+++ test/kmeanstest.py
@@ -13,7 +13,7 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.summaryDatas = self.loadSummaryDatas()
-        pass
+        
     def tearDown(self):
         pass
    
@@ -29,12 +29,31 @@ class Test(unittest.TestCase):
         for i in range(0,clusterdata.SummaryData.DIMENSIONS):
             self.assertTrue(maxes[i] > mins[i])
         
+        
     
     def testInitializeCentroids(self):
         
         km = kmeans.KmeansClusterer(4)
+        mins, maxes = km.initializeRanges(self.summaryDatas)
         centroids = km.initializeCentroids(self.summaryDatas)
         self.assertEquals(4,len(centroids))
+        
+        for centroid in centroids:
+            self.assertTrue(centroid.totalDist >= mins[clusterdata.TOTAL_DIST])
+            self.assertTrue(centroid.totalDist <= maxes[clusterdata.TOTAL_DIST])
+            
+            self.assertTrue(centroid.avgHR >= mins[clusterdata.AVG_HR])
+            self.assertTrue(centroid.avgHR <= maxes[clusterdata.AVG_HR])
+            
+            self.assertTrue(centroid.netGained >= mins[clusterdata.NET_GAINED])
+            self.assertTrue(centroid.netGained <= maxes[clusterdata.NET_GAINED])
+        
+            self.assertTrue(centroid.netLost >= mins[clusterdata.NET_LOST])
+            self.assertTrue(centroid.netLost <= maxes[clusterdata.NET_LOST])
+            
+            self.assertTrue(centroid.timeSeconds >= mins[clusterdata.TIME])
+            self.assertTrue(centroid.timeSeconds <= maxes[clusterdata.TIME])
+        
         
         
     
